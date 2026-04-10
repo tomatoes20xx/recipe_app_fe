99882d20d48c4b0da34a5a26f6f6165bdfdac563
tomatoes20xx <tomakacheishvilI@gmail.com>
2026-04-10 11:39:52 +0400

    android gradle fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,7 +66,7 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
